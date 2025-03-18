f2225cbd4f0b545d560405180c79fed38b61e49e
Gabriel Joaquim <gabriel.joaquim.5567@gmail.com>
2025-03-18 11:09:32 -0300

    feat: adição de output e arquivo aleatório

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { TextOverflowComponent } from "./text-overflow/text-overflow.component";
 import { BiscoitoComponent } from "./biscoito/biscoito.component";
 import { FrasesComponent } from "./frases/frases.component";
+import { AleatorioComponent } from "./aleatorio/aleatorio.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TextOverflowComponent, BiscoitoComponent, FrasesComponent],
+  imports: [RouterOutlet,TextOverflowComponent, BiscoitoComponent, FrasesComponent, AleatorioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pw3-input-output';
+
+  resultado : string = "";
+
+  processar(resultado:string){
+    this.resultado = resultado
+  }
 }
